0fd0def10b46554b5192de76e9d23ce55d87f3d2
wangjiahao <m13350768179@163.com>
2020-02-03 17:22:33 +0800

    demo

---
+++ src/main/java/com/example/gdemo/GdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GdemoApplication.class, args);
+    }
+
+}
