459bb2b8c3c2d9a4474aa996560021adc8abe6ef
wangjiahao-ls <55421614+wangjiahao-ls@users.noreply.github.com>
2020-02-05 22:46:51 +0800

    Update GdemoApplication.java

--- src/main/java/com/example/gdemo/GdemoApplication.java
+++ src/main/java/com/example/gdemo/GdemoApplication.java
@@ -2,12 +2,13 @@ package com.example.gdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GdemoApplication {
+public class GdemoApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GdemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GdemoApplication.class, args);
+	}
 
 }
