b4eea93cf1f42458b11d933298fc1197917b17bb
wangjiahao <m13350768179@163.com>
2020-02-06 17:27:30 +0800

    ceshi

--- src/main/java/com/example/gdemo/controller/TestController.java
+++ src/main/java/com/example/gdemo/controller/TestController.java
@@ -1,5 +1,9 @@
 package com.example.gdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class TestController {
     /**
@@ -7,6 +11,7 @@ public class TestController {
      * @return
      */
     @RequestMapping("/test")
+    @ResponseBody
     public String index() {
         return "index";
     }
